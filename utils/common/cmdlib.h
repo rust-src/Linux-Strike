f41debdaad1370f1ccccbe8277b6ed6dd5c66886
blackletum <blackletum@hotmail.com>
2025-04-07 18:08:50 -0400

    Update cmdlib.h
    fix gethourminute stuff

--- utils/common/cmdlib.h
+++ utils/common/cmdlib.h
@@ -20,9 +20,9 @@
 
 // This can go away when everything is in bin.
 #if defined( CMDLIB_NODBGLIB )
-	void Error( char const *pMsg, ... );
+void Error(char const* pMsg, ...);
 #else
-	#include "tier0/dbg.h"
+#include "tier0/dbg.h"
 #endif
 
 #include <stdio.h>
@@ -41,18 +41,18 @@
 
 
 // Tools should use this to fprintf data to files.
-void CmdLib_FPrintf( FileHandle_t hFile, const char *pFormat, ... );
-char* CmdLib_FGets( char *pOut, int outSize, FileHandle_t hFile );
+void CmdLib_FPrintf(FileHandle_t hFile, const char* pFormat, ...);
+char* CmdLib_FGets(char* pOut, int outSize, FileHandle_t hFile);
 
 
 // This can be set so Msg() sends output to hook functions (like the VMPI MySQL database),
 // but doesn't actually printf the output.
 extern bool g_bSuppressPrintfOutput;
 
-extern IBaseFileSystem *g_pFileSystem;
+extern IBaseFileSystem* g_pFileSystem;
 
 // These call right into the functions in filesystem_tools.h
-void				CmdLib_InitFileSystem( const char *pFilename, int maxMemoryUsage = 0 );
+void				CmdLib_InitFileSystem(const char* pFilename, int maxMemoryUsage = 0);
 void				CmdLib_TermFileSystem();	// GracefulExit calls this.
 CreateInterfaceFn	CmdLib_GetFileSystemFactory();
 
@@ -76,37 +76,42 @@ CreateInterfaceFn	CmdLib_GetFileSystemFactory();
 
 // set these before calling CheckParm
 extern int myargc;
-extern char **myargv;
+extern char** myargv;
 
-int Q_filelength (FileHandle_t f);
-int	FileTime (char *path);
+int Q_filelength(FileHandle_t f);
+int	FileTime(char* path);
 
-void	Q_mkdir( char *path );
+void	Q_mkdir(char* path);
 
-char *ExpandArg (char *path);	// expand relative to CWD
-char *ExpandPath (char *path);	// expand relative to gamedir
+char* ExpandArg(char* path);	// expand relative to CWD
+char* ExpandPath(char* path);	// expand relative to gamedir
 
-char *ExpandPathAndArchive (char *path);
+char* ExpandPathAndArchive(char* path);
 
-int		CheckParm (char *check);
+// Fills in pOut with "X hours, Y minutes, Z seconds". Leaves out hours or minutes if they're zero.
+void GetHourMinuteSecondsString(int nInputSeconds, char* pOut, int outLen);
 
-FileHandle_t	SafeOpenWrite ( const char *filename );
-FileHandle_t	SafeOpenRead ( const char *filename );
-void			SafeRead( FileHandle_t f, void *buffer, int count);
-void			SafeWrite( FileHandle_t f, void *buffer, int count);
 
-int		LoadFile ( const char *filename, void **bufferptr );
-void	SaveFile ( const char *filename, void *buffer, int count );
-qboolean	FileExists ( const char *filename );
 
-int 	ParseNum (char *str);
+int		CheckParm(char* check);
+
+FileHandle_t	SafeOpenWrite(const char* filename);
+FileHandle_t	SafeOpenRead(const char* filename);
+void			SafeRead(FileHandle_t f, void* buffer, int count);
+void			SafeWrite(FileHandle_t f, void* buffer, int count);
+
+int		LoadFile(const char* filename, void** bufferptr);
+void	SaveFile(const char* filename, void* buffer, int count);
+qboolean	FileExists(const char* filename);
+
+int 	ParseNum(char* str);
 
 // Do a printf in the specified color.
 #define CP_ERROR	stderr, 1, 0, 0, 1		// default colors..
 #define CP_WARNING	stderr, 1, 1, 0, 1		
 #define CP_STARTUP	stdout, 0, 1, 1, 1		
 #define CP_NOTIFY	stdout, 1, 1, 1, 1
-void ColorPrintf( FILE *pFile, bool red, bool green, bool blue, bool intensity, char const *pFormat, ... );
+void ColorPrintf(FILE* pFile, bool red, bool green, bool blue, bool intensity, char const* pFormat, ...);
 
 // Initialize spew output.
 void InstallSpewFunction();
@@ -118,25 +123,25 @@ void InstallAllocationFunctions();
 // This shuts down mgrs that use threads gracefully. If you just call exit(), the threads can
 // get in a state where you can't tell if they are shutdown or not, and it can stall forever.
 typedef void (*CleanupFn)();
-void CmdLib_AtCleanup( CleanupFn pFn );	// register a callback when Cleanup() is called.
+void CmdLib_AtCleanup(CleanupFn pFn);	// register a callback when Cleanup() is called.
 void CmdLib_Cleanup();
 
 // entrypoint if chaining spew functions
-unsigned short SetConsoleTextColor( int red, int green, int blue, int intensity );
-void RestoreConsoleTextColor( unsigned short color );
+unsigned short SetConsoleTextColor(int red, int green, int blue, int intensity);
+void RestoreConsoleTextColor(unsigned short color);
 
 class CCmdLibStandardLoggingListener : public ILoggingListener
 {
 public:
-	virtual void Log( const LoggingContext_t *pContext, const tchar *pMessage );
+	virtual void Log(const LoggingContext_t* pContext, const tchar* pMessage);
 };
 
 class CCmdLibFileLoggingListener : public ILoggingListener
 {
 public:
 	CCmdLibFileLoggingListener();
-	virtual void Log( const LoggingContext_t *pContext, const tchar *pMessage );
-	void Open( char const *pFilename );
+	virtual void Log(const LoggingContext_t* pContext, const tchar* pMessage);
+	void Open(char const* pFilename);
 	void Close();
 
 private:
@@ -146,37 +151,37 @@ private:
 extern CCmdLibStandardLoggingListener g_CmdLibOutputLoggingListener;
 extern CCmdLibFileLoggingListener g_CmdLibFileLoggingListener;
 
-char *COM_Parse (char *data);
+char* COM_Parse(char* data);
 
 extern	char		com_token[1024];
 
-char *copystring(const char *s);
+char* copystring(const char* s);
 
-void	CreatePath( char *path );
-void	QCopyFile( char *from, char *to );
-void	SafeCreatePath( char *path );
+void	CreatePath(char* path);
+void	QCopyFile(char* from, char* to);
+void	SafeCreatePath(char* path);
 
 extern	qboolean		archive;
 extern	char			archivedir[1024];
 
 extern	qboolean verbose;
 
-void qprintf( char *format, ... );
+void qprintf(char* format, ...);
 
-void ExpandWildcards (int *argc, char ***argv);
+void ExpandWildcards(int* argc, char*** argv);
 
-void CmdLib_AddBasePath( const char *pBasePath );
-void CmdLib_AddNewSearchPath( const char *pBasePath );
-bool CmdLib_HasBasePath( const char *pFileName, int &pathLength );
-int CmdLib_GetNumBasePaths( void );
-const char *CmdLib_GetBasePath( int i );
+void CmdLib_AddBasePath(const char* pBasePath);
+void CmdLib_AddNewSearchPath(const char* pBasePath);
+bool CmdLib_HasBasePath(const char* pFileName, int& pathLength);
+int CmdLib_GetNumBasePaths(void);
+const char* CmdLib_GetBasePath(int i);
 
 extern bool g_bStopOnExit;
 
 // for compression routines
 typedef struct
 {
-	byte	*data;
+	byte* data;
 	int		count;
 } cblock_t;
 
